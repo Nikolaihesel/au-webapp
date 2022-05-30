2291af26b459f959af3d742dc4a243a76eeefbfc
Nikolaihesel <Nikolaihesel@icloud.com>
2022-05-30 12:14:06 +0200

    stopRefresh

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import Home from "./Components/Home"
 import Logo from './assets/autonomuosUnits_logo.png';
 import { useAuthContext } from './hooks/useAuthContext'
 import './app.scss';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import {useState, useEffect} from 'react'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useLogout } from './hooks/useLogout';
 
+
+
 function App() {
+
+  const stopRefresh = (e) => {
+    e.preventDefault();
+  }
+
   const {authIsReady, user} = useAuthContext()
   const {logout} = useLogout();
   return (
 
-    <div className="App">
+    <div className="App" onChange={stopRefresh}>
 { <img className="logo" src={Logo} height="70"  onClick={logout}  /> }
 {authIsReady && (
-  <BrowserRouter>
+  <HashRouter>
   <Switch>
     <Route exact path="/" >
-      {!user && <Redirect to="/login" />}
-      {user && <Home /> } 
+      {!user && <Redirect to="/login"  component={LoginPage}/>}
+      {user && <Home component={Home} />  } 
     </Route>
      <Route path="/login">
               {user && <Redirect to="/" />}
               {!user && <LoginPage />}
             </Route>
   </Switch>
-  </BrowserRouter>
+  </HashRouter>
 )}
   
     </div>
